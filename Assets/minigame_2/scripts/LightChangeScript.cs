cd81ffd3aef577d371add4c96a6cd24bff1cc9bd
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-03-16 12:02:05 +0000

    Code: behaviour on progress bar now depends on phone tilting instead of being automatic.

--- Assets/minigame_2/scripts/LightChangeScript.cs
+++ Assets/minigame_2/scripts/LightChangeScript.cs
@@ -7,37 +7,22 @@ public class LightChangeScript : MonoBehaviour
     public Slider slider;
     public Sprite[] pictureFrames = new Sprite[5];
     private GameObject picture;
-    private float timer = 0;
-    private float angleDegrees = 45;
-    private float offset = 1;
-    public float changingSpeed = 0.01f;
+    private int index = 0;
+    private float moveSpeed = 0.02f;
 
     void Start()
     {
         slider = gameObject.GetComponent<Slider>();
         picture = GameObject.Find("Changing Oil Paint");
-        Debug.Log(slider.value);
     }
 
     void Update()
     {
-        int index;
-        if (timer < changingSpeed)
-            timer += Time.deltaTime;
-        else
-        {           
-            if (angleDegrees == 90)
-                offset = -1;
-            else if (angleDegrees == 0)
-                offset = 1;       
-            
-            angleDegrees += offset;
-            slider.value = Mathf.Pow(Mathf.Sin(angleDegrees * Mathf.PI / 180) , 2);
-
-            index = (int)(slider.value * 4);
-            picture.GetComponent<SpriteRenderer>().sprite = pictureFrames[index];
-            timer = 0;
-        }
-        
+        Screen.orientation = ScreenOrientation.Portrait;
+        slider.value = Mathf.Clamp(slider.value + Input.acceleration.x * moveSpeed, 0, 1);
+        Debug.Log("ACCEL: " + Input.acceleration.x);
+        Debug.Log("VALUE: " + slider.value);
+        index = (int)(slider.value * 4);
+        picture.GetComponent<SpriteRenderer>().sprite = pictureFrames[index];
     }
 }
