fde9ee60c6bdcdd382d8ac8786abedbc4ac91d68
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-04-14 10:23:46 +0100

    Code: was able to create a circular progress bar loader between the light scene and humidity intro. However I still can't destroy it.

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -6,45 +6,78 @@ using UnityEngine.SceneManagement;
 
 public class LightController : MonoBehaviour
 {
-    public Slider slider;
+    public Slider lightSlider;
+	public Slider loadingSlider;
+	public Slider loadingSliderClone;
+	public Button fingerprint;
+    public GameObject lightPaint;
+	public Canvas canvas;
     public Sprite[] pictureFrames = new Sprite[5];
-    public GameObject picture;
+	public bool isFingerprintClicked = false;
+	public bool wasLoadingRendered = false;
     public float moveSpeed = 10.0f;
-    private KeyValuePair<float, float>[] ranges;
-    public int correctRange = 2;
-    public int currentRange = 3;
+    public int lightCurrentFrame = 0;
+    public int lightCorrectFrame = 2;
+	public float timer = 0.0f;
+    private KeyValuePair<float, float>[] lightRanges;
 
     void Start()
     {
-        slider = gameObject.GetComponent<Slider>();
+        lightRanges = new KeyValuePair<float, float>[5];
+        lightRanges[0] = new KeyValuePair<float, float>(20.0f, 34.9f);
+        lightRanges[1] = new KeyValuePair<float, float>(35.0f, 49.9f);
+        lightRanges[2] = new KeyValuePair<float, float>(50.0f, 149.9f);
+        lightRanges[3] = new KeyValuePair<float, float>(150.0f, 324.9f);
+        lightRanges[4] = new KeyValuePair<float, float>(325.0f, 500.0f);
 
-        ranges = new KeyValuePair<float, float>[5];
-        ranges[0] = new KeyValuePair<float, float>(20.0f, 34.9f);
-        ranges[1] = new KeyValuePair<float, float>(35.0f, 49.9f);
-        ranges[2] = new KeyValuePair<float, float>(50.0f, 149.9f);
-        ranges[3] = new KeyValuePair<float, float>(150.0f, 324.9f);
-        ranges[4] = new KeyValuePair<float, float>(325.0f, 500.0f);
+		lightSlider.value = lightRanges[lightCurrentFrame].Key;
+		fingerprint.onClick.AddListener(onFingerprintClick);
     }
 
     void Update()
     {   
-        // If the users are touching the screen to lock the result, don't move the slider
-        if (Input.touchCount != 0)
-            return ;
+		if (isFingerprintClicked)
+			checkSuccess();
+		else
+			lightSlider.value = Mathf.Clamp(lightSlider.value + Input.acceleration.x * moveSpeed, lightSlider.minValue, lightSlider.maxValue);	
 
-        //The sliders value will adjust according to the mobile device rotation on the x axis
-        slider.value = Mathf.Clamp(slider.value + Input.acceleration.x * moveSpeed, slider.minValue, slider.maxValue);
-        for (int i = 0; i < 5; i++)
-        {
-            if (slider.value >= ranges[i].Key && slider.value <= ranges[i].Value)
-            {
-                currentRange = i;
-                picture.GetComponent<SpriteRenderer>().sprite = pictureFrames[i];
-                break;                
-            }
-        }
-
-        if (correctRange == currentRange)
-            SceneManager.LoadScene("HumidityIntro");
+		for (int i = 0; i < 5; i++)
+		{
+			if (between(lightSlider.value, lightRanges[i].Key, lightRanges[i].Value))
+			{
+				lightCurrentFrame = i;
+				lightPaint.GetComponent<SpriteRenderer>().sprite = pictureFrames[i];
+				break;                
+			}
+		}			
     }
+
+	void checkSuccess()
+	{
+		if (!wasLoadingRendered)
+		{
+			loadingSliderClone = Instantiate(loadingSlider, canvas.transform);
+			wasLoadingRendered = true;
+		}
+		timer += Time.deltaTime;
+		loadingSliderClone.value += Time.deltaTime;
+		if (timer < 3.0f)
+			return ;
+		if (lightCurrentFrame == lightCorrectFrame)
+			SceneManager.LoadScene("HumidityIntro");
+		timer = 0.0f;
+		isFingerprintClicked = false;
+		wasLoadingRendered = false;
+		Destroy(loadingSliderClone, 0.0f);
+	}
+
+	bool between(float n, float a, float b)
+	{
+		return (n >= a && n <= b);
+	}
+
+	void onFingerprintClick()
+	{
+		isFingerprintClicked = !isFingerprintClicked;
+	}
 }
