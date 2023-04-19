8c41893051f2bb515c7ab3722c8b1a533e23784f
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-04-19 16:35:18 +0100

    Code: introduced 3 fingerprints needed for confirmation of the input.

--- Assets/Scripts/OverlayController.cs
+++ Assets/Scripts/OverlayController.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -11,21 +11,82 @@ public class OverlayController : MonoBehaviour
 	private Slider loadingClone;
 	public Slider lightSlider;
 	public Button[] fingerprints = new Button[3];
+	public bool[] wasFingerprintClicked = new bool[3];
+	public bool wasLoadingRendered = false; 
+	public Sprite activeSprite;
+	public Sprite inactiveSprite;
 
     public void togglePanel()
 	{
-		overlay.SetActive(true);
-		loadingClone = Instantiate(loading, canvas.transform);
+		overlay.SetActive(!overlay.activeSelf);
+	}
+
+	public void Start()
+	{
+		fingerprints[0].onClick.AddListener(onFirstFingerprintClick);
+		fingerprints[1].onClick.AddListener(onSecondFingerprintClick);
+		fingerprints[2].onClick.AddListener(onThirdFingerprintClick);
 	}
 
 	public void Update()
 	{
+		// This script should only run if the primary fingerprint was toggled
 		if (!overlay.activeSelf)
 			return;
+		if (!wasLoadingRendered)
+		{
+			// If one of the buttons wasn't clicked, abort
+			if (!wasFingerprintClicked.All(x => x == true))
+				return ;
+		
+			// Else display the loading slider
+			loadingClone = Instantiate(loading, canvas.transform);
+			wasLoadingRendered = true;
+		}
+
+		// Wait 3 seconds for validation
 		if (loadingClone.value < 3.0f)
 			return ;
+		
+		// If the user locked the input in the right range, load next screen
 		if (lightSlider.GetComponent<LightController>().hasEnteredCorrectRange())
 			SceneManager.LoadScene("PlayerSwap1");
-		overlay.SetActive(false);
+		
+		//Otherwise, untoggle the overlay and reset variables
+		reset();
+	}
+
+	void onFirstFingerprintClick()
+	{
+		wasFingerprintClicked[0] = !wasFingerprintClicked[0];
+		if (wasFingerprintClicked[0])
+			fingerprints[0].GetComponent<Image>().sprite = activeSprite;
+		else
+			fingerprints[0].GetComponent<Image>().sprite = inactiveSprite;
+	}
+	void onSecondFingerprintClick()
+	{
+		wasFingerprintClicked[1] = !wasFingerprintClicked[1];
+		if (wasFingerprintClicked[1])
+			fingerprints[1].GetComponent<Image>().sprite = activeSprite;
+		else
+			fingerprints[1].GetComponent<Image>().sprite = inactiveSprite;
+	}
+	void onThirdFingerprintClick()
+	{
+		wasFingerprintClicked[2] = !wasFingerprintClicked[2];
+		if (wasFingerprintClicked[2])
+			fingerprints[2].GetComponent<Image>().sprite = activeSprite;
+		else
+			fingerprints[2].GetComponent<Image>().sprite = inactiveSprite;
+	}
+
+	void reset()
+	{
+		togglePanel();
+		Array.ForEach(fingerprints, button => button.GetComponent<Image>().sprite = inactiveSprite);
+		Array.Fill(wasFingerprintClicked, false);
+		wasLoadingRendered = false;
+		loadingClone = null;
 	}
 }
