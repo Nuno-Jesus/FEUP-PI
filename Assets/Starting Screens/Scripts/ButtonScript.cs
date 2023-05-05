1f6548931fe6ed5b98b66d4b50ed8d275c1280b9
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-05-05 11:26:43 +0100

    Refactoring + UI: removed duplicate button managers, increased scratch mask size

--- Assets/Starting Screens/Scripts/ButtonScript.cs
+++ Assets/Starting Screens/Scripts/ButtonScript.cs
@@ -8,8 +8,8 @@ public class ButtonScript : MonoBehaviour
 {
 	public Button button;
 	public string nextScene;
-	public bool isPlayerSwap;
-	public SceneLoader sceneLoader;
+	public bool isMinigameSwap;
+	public SceneLoader MinigameLoader;
 
 	void Start()
 	{
@@ -19,8 +19,8 @@ public class ButtonScript : MonoBehaviour
 
     public void onClick()
 	{
-		if (isPlayerSwap)			
-			sceneLoader.loadNextMinigame();
+		if (isMinigameSwap)			
+			MinigameLoader.loadNextMinigame();
 		else
 			SceneManager.LoadScene(nextScene);
 	}
@@ -28,22 +28,22 @@ public class ButtonScript : MonoBehaviour
 	public void onMinigame1()
 	{
 		Debug.Log("Minigame 1");
-		sceneLoader.Minigames = null;
-		sceneLoader.Minigames = new string[4];
-		sceneLoader.Minigames[0] = "InsectsStartScene";
-		sceneLoader.Minigames[1] = "LightStartScene";
-		sceneLoader.Minigames[2] = "EarthquakeStartScene";
-		sceneLoader.Minigames[3] = "EraserStartScene";
+		MinigameLoader.Minigames = null;
+		MinigameLoader.Minigames = new string[4];
+		MinigameLoader.Minigames[0] = "InsectsStartScene";
+		MinigameLoader.Minigames[1] = "LightStartScene";
+		MinigameLoader.Minigames[2] = "EarthquakeStartScene";
+		MinigameLoader.Minigames[3] = "EraserStartScene";
 	}
 
 	public void onMinigame2()
 	{
 		Debug.Log("Minigame 2");
-		sceneLoader.Minigames = null;
-		sceneLoader.Minigames = new string[4];
-		sceneLoader.Minigames[0] = "LightStartScene";
-		sceneLoader.Minigames[1] = "InsectsStartScene";
-		sceneLoader.Minigames[2] = "EarthquakeStartScene";
-		sceneLoader.Minigames[3] = "EraserStartScene";
+		MinigameLoader.Minigames = null;
+		MinigameLoader.Minigames = new string[4];
+		MinigameLoader.Minigames[0] = "LightStartScene";
+		MinigameLoader.Minigames[1] = "InsectsStartScene";
+		MinigameLoader.Minigames[2] = "EarthquakeStartScene";
+		MinigameLoader.Minigames[3] = "EraserStartScene";
 	}
 }
