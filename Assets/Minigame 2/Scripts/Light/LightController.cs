c3b1084d70ec811d738f8b9500b1a0ccd1f14c84
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-05-26 14:57:11 +0100

    UI: added a yellow outline to the Light Game correct paint.

--- Assets/Minigame 2/Scripts/Light/LightController.cs
+++ Assets/Minigame 2/Scripts/Light/LightController.cs
@@ -11,31 +11,38 @@ public class LightController : MonoBehaviour
 	public AudioSource hotDetectorSoundSource;
     public GameObject lightPaint;
 	public GameObject overlay;
+	public GameObject outline;
     public Sprite[] pictureFrames = new Sprite[5];
     public const float moveSpeed = 10.0f;
-    public int paintCurrentFrame = 0;
+    public int paintCurrentFrame = 3;
     public const int paintCorrectFrame = 2;
     private KeyValuePair<float, float>[] lightRanges;
 
     void Start()
     {
         lightRanges = new KeyValuePair<float, float>[5];
-        lightRanges[0] = new KeyValuePair<float, float>(20.0f, 34.9f);
-        lightRanges[1] = new KeyValuePair<float, float>(35.0f, 49.9f);
-        lightRanges[2] = new KeyValuePair<float, float>(50.0f, 149.9f);
-        lightRanges[3] = new KeyValuePair<float, float>(150.0f, 324.9f);
+        lightRanges[0] = new KeyValuePair<float, float>(20.0f, 34.99f);
+        lightRanges[1] = new KeyValuePair<float, float>(35.0f, 49.99f);
+        lightRanges[2] = new KeyValuePair<float, float>(50.0f, 149.99f);
+        lightRanges[3] = new KeyValuePair<float, float>(150.0f, 324.99f);
         lightRanges[4] = new KeyValuePair<float, float>(325.0f, 500.0f);
 
-		lightSlider.value = lightRanges[paintCurrentFrame].Key;
+		lightSlider.value = 250.0f;
     }
 
     void Update()
     {   
 		//If the user entered the correct range and the sound wasn't playing so far, play it
 		if (hasEnteredCorrectRange() && !hotDetectorSoundSource.isPlaying)
+		{
+			outline.SetActive(true);
 			hotDetectorSoundSource.Play();
+		}
 		else if (!hasEnteredCorrectRange())
+		{
+			outline.SetActive(false);
 			hotDetectorSoundSource.Stop();
+		}
 
 		//If the user clicked the fingerprint and opened the overlay, stop the paint update
 		if (overlay.activeSelf)
